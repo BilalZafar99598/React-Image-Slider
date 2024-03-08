a7ee7bec5ed460d443a8ea5708cf8743c8242c62
BilalZafar99598 <bilal.halfur@gmail.com>
2024-03-08 20:56:42 +0500

    React Image Slider from Scratch

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ImageSlider from "./image-slider/ImageSlider"
+
+const App = () => {
+  return (
+    <div>
+        <ImageSlider  url={"https://picsum.photos/v2/list"} limit={'10'}/>
+    </div>
+  )
+}
+
+export default App
